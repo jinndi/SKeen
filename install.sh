717bdb628466258c5afa87fedc72112411f574e9
Jinndi <alncores@gmail.com>
2026-04-07 21:07:40 +0300

    Bump version

--- install.sh
+++ install.sh
@@ -29,7 +29,7 @@ MODULES_OS_DIR="/lib/modules"
 MODULES_ENTWARE_DIR="${ENTWARE_DIR}/lib/modules"
 
 SKEEN_NAME="SKeen"
-SKEEN_VERSION="4.4.2"
+SKEEN_VERSION="4.4.3"
 SKEEN_PROC="skeen"
 SKEEN_SCRIPT="${ENTWARE_DIR}/bin/${SKEEN_PROC}"
 SKEEN_SCRIPT_URL="https://github.com/jinndi/SKeen/releases/latest/download/skeen"
